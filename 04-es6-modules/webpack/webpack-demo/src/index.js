eed1d3e3d3e099b7ef501c4782f6730cb39f450d
RJ Shoemaker <raymond.shoemaker@spigroup.com>
2022-02-04 10:06:35 -0500

    ediits

--- 04-es6-modules/webpack/webpack-demo/src/index.js
+++ 04-es6-modules/webpack/webpack-demo/src/index.js
@@ -1,9 +1,10 @@
 import _ from 'lodash'
+import myName from './myName'
 
 function component() {
   const element = document.createElement('div')
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  element.textContent = myName('RJ')
 
   return element
 }
---
+++ 04-es6-modules/webpack/webpack-demo/src/myName.js
@@ -0,0 +1,5 @@
+const myName = (name) => {
+  return `Hi, my name is ${name}`
+}
+
+export default myName
